38a53bc8e53ba9b1d81da0f41f489a466bd7c2c7
elytrafae <48065803+elytrafae@users.noreply.github.com>
2026-06-15 20:12:19 +0300

    Somewhat working HUD test...?

--- src/main/java/xyz/elytrafae/mc/polynutrition/HUDManager.java
+++ src/main/java/xyz/elytrafae/mc/polynutrition/HUDManager.java
@@ -5,6 +5,7 @@ import eu.pb4.polymer.virtualentity.api.attachment.EntityAttachment;
 import eu.pb4.polymer.virtualentity.api.elements.MarkerElement;
 import eu.pb4.polymer.virtualentity.api.elements.TextDisplayElement;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetPassengersPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Display;
 import org.joml.Vector3f;
@@ -16,23 +17,16 @@ public class HUDManager {
     private ElementHolder holder;
     private EntityAttachment attachment;
 
-    private MarkerElement testArmorStand;
-
     public HUDManager(ServerPlayer entity) {
         text = new TextDisplayElement();
         text.setText(Component.literal("Hello!"));
         text.setBillboardMode(Display.BillboardConstraints.CENTER);
         text.setSeeThrough(true);
-        text.setTranslation(new Vector3f(1f, -0.5f, 1f));
-        text.setScale(new Vector3f(2f, 2f, 2f));
-
-        testArmorStand = new MarkerElement();
-        testArmorStand.setCustomName(Component.literal("Test Armor Stand!"));
-        testArmorStand.setCustomNameVisible(true);
+        text.setTranslation(new Vector3f(2f, -1f, -3f));
+        text.setScale(new Vector3f(1f, 1f, 1f));
 
         holder = new ElementHolder();
         holder.addPassengerElement(text);
-        holder.addPassengerElement(testArmorStand);
         setPlayer(entity);
     }
 
@@ -43,14 +37,12 @@ public class HUDManager {
         this.player = player;
 
         updateText();
-        //text.setInitialPosition(player.getEyePos());
 
         attachment = EntityAttachment.ofTicking(holder, player);
         attachment.startWatching(player);
 
-        // EntityPassengersSetS2CPacket packet = new EntityPassengersSetS2CPacket()
-
-        // TODO: Make the text ride the player...?
+        ClientboundSetPassengersPacket packet = new ClientboundSetPassengersPacket(player);
+        player.connection.send(packet);
         player.sendSystemMessage(Component.literal("You should see text!"));
     }
 
